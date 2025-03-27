78e6bd600e709a225d27ebb4397e8bacfb25c4a5
вфывфы <hdvhdvwww@gmail.com>
2025-03-27 11:34:15 +0300

    feat: add tailwind css & react-router

---
+++ frontend/src/components/Page404/Page404.tsx
@@ -0,0 +1,11 @@
+export default function Page404() {
+  return (
+    <div className=" w-full h-full flex justify-center items-center flex-col text-white ">
+      <h1 className="text-4xl text-center mt-5 mb-4">Ошибка 404</h1>
+      <h3 className="text-2l text-center">Не найдено</h3>
+      <p className="underline text-3xl text-center">
+        Данной страницы не существует
+      </p>
+    </div>
+  );
+}
